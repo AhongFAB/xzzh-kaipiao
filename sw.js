8bc68442d34bef2cf802f443f8fa1a593656105f
AhongFAB <ahongwenbin@coze.email>
2026-05-23 15:09:48 +0800

    v6: 替代数量取整数，支持新进项格式

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kaipiao-v5';
+const CACHE_NAME = 'kaipiao-v6';
 const ASSETS = ['./index.html'];
 self.addEventListener('install', function(event) { event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); })); self.skipWaiting(); });
 self.addEventListener('activate', function(event) { event.waitUntil(caches.keys().then(function(keys) { return Promise.all(keys.filter(function(key) { return key !== CACHE_NAME; }).map(function(key) { return caches.delete(key); })); })); self.clients.claim(); });
