0b8a533ade91c2d3ab97596765c7f0d28efbef41
AhongFAB <ahongwenbin@coze.email>
2026-05-23 13:45:31 +0800

    chore: 升级SW缓存到v2

--- sw.js
+++ sw.js
@@ -1,34 +1,25 @@
-const CACHE_NAME = 'kaipiao-v1';
-const ASSETS = [
-  '/xzzh-kaipiao/',
-  '/xzzh-kaipiao/index.html',
-  '/xzzh-kaipiao/manifest.json'
-];
+const CACHE_NAME = 'kaipiao-v2';
+const ASSETS = ['./index.html'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); }));
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(keys) {
+        return Promise.all(keys.filter(function(key) { return key !== CACHE_NAME; }).map(function(key) { return caches.delete(key); }));
+    }));
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(response => {
-      if (response && response.status === 200) {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-      }
-      return response;
-    }))
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+        return response || fetch(event.request).then(function(fetchResponse) {
+            return caches.open(CACHE_NAME).then(function(cache) {
+                if (event.request.url.startsWith('http')) { cache.put(event.request, fetchResponse.clone()); }
+                return fetchResponse;
+            });
+        });
+    }).catch(function() { return caches.match('./index.html'); }));
 });
